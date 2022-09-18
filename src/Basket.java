18c70fd8ba469ed4c8dfeaf27b95d377b0090ae5
Svetlana K <kov_sv@mail.ru>
2022-09-18 13:55:51 +1000

    First commit

---
+++ src/Basket.java
@@ -0,0 +1,79 @@
+import java.io.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Basket {
+    private static List<String> goods;
+    private static List<Integer> price;
+    private static Map<Integer, Integer> allGoods = new HashMap<>();
+
+    public Basket(List<String> goods, List<Integer> price) {
+        this.goods = goods;
+        this.price = price;
+    }
+
+    List<String> getGoods() {
+        return goods;
+    }
+
+    List<Integer> getPrice() {
+        return price;
+    }
+
+    public Map<Integer, Integer> getAllGoods() {
+        return allGoods;
+    }
+
+    public void addToCart(int numProduct, int amount) {
+        if (allGoods.containsKey(numProduct)) {
+            allGoods.put(numProduct, allGoods.get(numProduct) + amount);
+        } else {
+            allGoods.put(numProduct, amount);
+        }
+    }
+
+    public void printCart() {
+        for (int i = 0; i < getGoods().size(); i++) {
+            if (getAllGoods().get(i) != null) {
+                System.out.println((i + 1) + ". " + getGoods().get(i) + ": " + (getGoods().get(i))
+                        + " ед. " + " x " + getPrice().get(i) + " руб/ед. " + " = "
+                        + getAllGoods().get(i) * getPrice().get(i) + " руб. ");
+            }
+        }
+    }
+
+    public void saveTxt(File textFile) throws IOException {
+        try (PrintWriter out = new PrintWriter(textFile)) {
+            for (int i = 0; i < getGoods().size(); i++) {
+                if (getAllGoods().get(i) != null) {
+                    String tmp = getGoods().get(i) + ": " + getAllGoods().get(i) + " ед. " + " по цене " + getPrice().get(i) + " руб./ед. ";
+                    out.print(tmp);
+                    out.print('\n');
+                }
+            }
+        }
+        System.out.println("Информация успешно сохранена");
+    }
+
+    public static Basket loadFromTxtFile(File textFile) {
+        File file = new File(textFile.toURI());
+        try (FileReader fileReader = new FileReader(file);) {
+            BufferedReader reader = new BufferedReader(fileReader);
+            String line = reader.readLine();
+            int i = 0;
+            while (line != null) {
+                System.out.println(line);
+                String[] parse = line.split(" ");
+                allGoods.put(i, Integer.valueOf(parse[1]));
+                i++;
+                line = reader.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,58 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Basket cart = new Basket(List.of("Хлеб", "Масло", "Молоко", "Яйца"), List.of(45, 130, 100, 120));
+        System.out.println("Список доступных товаров: ");
+        for (int i = 0; i < cart.getGoods().size(); i++) {
+            System.out.println((i + 1) + ". " + cart.getGoods().get(i) + " по цене: " + cart.getPrice().get(i) + " руб./ед.");
+        }
+
+        Scanner scanner = new Scanner(System.in);
+        int costCart = 0;
+
+        while (true) {
+            System.out.println("Введите номер товара и его количество.");
+            System.out.println("Чтобы посмотреть список выбранных товаров в корзине, введите load.");
+            System.out.println("По заврешении заполнения корзины и расчета стоимости покупок нажмите end.");
+            String input = scanner.nextLine();
+
+            if ("end".equals(input)) {
+                for (int i = 0; i < cart.getAllGoods().size(); i++) {
+                    if (cart.getAllGoods().get(i) == null) {
+                        costCart = cart.getAllGoods().get(i) * cart.getPrice().get(i);
+                    } else {
+                        costCart += cart.getAllGoods().get(i) * cart.getPrice().get(i);
+                    }
+                }
+                cart.printCart();
+                System.out.println("Итого товароов  на сумму: " + costCart + " руб.");
+                break;
+            }
+            if ("load".equals(input)) {
+                cart.loadFromTxtFile(new File("cart.txt"));
+                System.out.println("Информация загружена");
+                continue;
+            }
+            String[] parse = input.split(" ");
+            if (parse.length != 2) {
+                System.out.println("Необходимо ввести 2 значения: номер товара и его количество. Вы ввели: " + parse.length);
+                continue;
+            } else {
+                try {
+                    int numProduct = Integer.parseInt(parse[0]) - 1;
+                    int amount = Integer.parseInt(parse[1]);
+                    cart.addToCart(numProduct, amount);
+                    cart.saveTxt(new File("cart.txt"));
+                } catch (NumberFormatException e) {
+                    System.out.println("Введены некорректные данные: необходимо ввести числовые значения");
+                    continue;
+                }
+            }
+        }
+    }
+}
