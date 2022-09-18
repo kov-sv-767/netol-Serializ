5082408497ab60f779433f360d69eb7e8c554192
Svetlana K <kov_sv@mail.ru>
2022-09-18 16:25:02 +1000

    First commit in branch serial

--- src/Basket.java
+++ src/Basket.java
@@ -3,25 +3,25 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class Basket {
+public class Basket implements Serializable {
     private static List<String> goods;
     private static List<Integer> price;
-    private static Map<Integer, Integer> allGoods = new HashMap<>();
+    private static final Map<Integer, Integer> allGoods = new HashMap<>();
 
     public Basket(List<String> goods, List<Integer> price) {
-        this.goods = goods;
-        this.price = price;
+        Basket.goods = goods;
+        Basket.price = price;
     }
 
-    List<String> getGoods() {
+    static List<String> getGoods() {
         return goods;
     }
 
-    List<Integer> getPrice() {
+    static List<Integer> getPrice() {
         return price;
     }
 
-    public Map<Integer, Integer> getAllGoods() {
+    public static Map<Integer, Integer> getAllGoods() {
         return allGoods;
     }
 
@@ -76,4 +76,29 @@ public class Basket {
         }
         return null;
     }
+
+    public Basket() {
+    }
+
+    public void saveBin(File file) throws IOException {
+        ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(file));//("basket.bin")
+        //Basket outBin = new Basket():
+        out.writeObject(new Basket());
+        out.close();
+    }
+
+    public static void loadFromBinFile(File file) throws IOException, ClassNotFoundException {
+        ObjectInputStream in = new ObjectInputStream(new FileInputStream(file));
+        Basket inBin = (Basket) in.readObject();
+        for (int i = 0; i < getGoods().size(); i++) {
+            if (getAllGoods().get(i) != null) {
+                System.out.println(getGoods().get(i) + ": " + getAllGoods().get(i)
+                        + " ед. " + " по цене " + getPrice().get(i) + " руб./ед. ");
+            }
+        }
+        in.close();
+    }
+
+
 }
+
--- src/Main.java
+++ src/Main.java
@@ -5,28 +5,27 @@ import java.util.Scanner;
 
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
         Basket cart = new Basket(List.of("Хлеб", "Масло", "Молоко", "Яйца"), List.of(45, 130, 100, 120));
         System.out.println("Список доступных товаров: ");
-        for (int i = 0; i < cart.getGoods().size(); i++) {
-            System.out.println((i + 1) + ". " + cart.getGoods().get(i) + " по цене: " + cart.getPrice().get(i) + " руб./ед.");
+        for (int i = 0; i < Basket.getGoods().size(); i++) {
+            System.out.println((i + 1) + ". " + Basket.getGoods().get(i) + " по цене: " + Basket.getPrice().get(i) + " руб./ед.");
         }
 
         Scanner scanner = new Scanner(System.in);
         int costCart = 0;
 
         while (true) {
-            System.out.println("Введите номер товара и его количество.");
-            System.out.println("Чтобы посмотреть список выбранных товаров в корзине, введите load.");
+            System.out.println("Введите номер товара и его количество.Для просмотра корзины введите load.");
             System.out.println("По заврешении заполнения корзины и расчета стоимости покупок нажмите end.");
             String input = scanner.nextLine();
 
             if ("end".equals(input)) {
-                for (int i = 0; i < cart.getAllGoods().size(); i++) {
-                    if (cart.getAllGoods().get(i) == null) {
-                        costCart = cart.getAllGoods().get(i) * cart.getPrice().get(i);
+                for (int i = 0; i < Basket.getAllGoods().size(); i++) {
+                    if (Basket.getAllGoods().get(i) == null) {
+                        costCart = Basket.getAllGoods().get(i) * Basket.getPrice().get(i);
                     } else {
-                        costCart += cart.getAllGoods().get(i) * cart.getPrice().get(i);
+                        costCart += Basket.getAllGoods().get(i) * Basket.getPrice().get(i);
                     }
                 }
                 cart.printCart();
@@ -34,7 +33,7 @@ public class Main {
                 break;
             }
             if ("load".equals(input)) {
-                cart.loadFromTxtFile(new File("cart.txt"));
+                Basket.loadFromBinFile(new File("basket.bin"));
                 System.out.println("Информация загружена");
                 continue;
             }
@@ -47,7 +46,7 @@ public class Main {
                     int numProduct = Integer.parseInt(parse[0]) - 1;
                     int amount = Integer.parseInt(parse[1]);
                     cart.addToCart(numProduct, amount);
-                    cart.saveTxt(new File("cart.txt"));
+                    cart.saveBin(new File("basket.bin"));
                 } catch (NumberFormatException e) {
                     System.out.println("Введены некорректные данные: необходимо ввести числовые значения");
                     continue;
